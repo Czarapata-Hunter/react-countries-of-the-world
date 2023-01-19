2fc1799c89237a565c981f86ad0ff78713f67d0e
Hunter Czarapata <czarapata.hunter@gmail.com>
2023-01-18 16:44:04 -0800

    Loading functioning and working

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,15 +4,41 @@ import './Main.css';
 import CountryCard from '../CountryCard/CountryCard.js';
 
 export default function Main() {
-  const { countries, error } = useCountries();
+  const { countries, error, isLoading } = useCountries();
+  const [continent, setContinent] = React.useState('all');
+  const continents = [...new Set(countries.map(({ continent }) => continent))];
+  const filtered = countries.filter(
+    (country) => country.continent === continent || continent === 'all'
+  );
 
+  if (isLoading && !error) {
+    return (
+      <article>
+        <p className="loading">Loading...</p>
+      </article>
+    );
+  }
   //   console.log(countries);
   return (
     <main className="main">
-      <p>{error}</p>
-      {countries.map((country) => (
-        <CountryCard key={country.name} {...country} />
-      ))}
+      <div className="drop-down">
+        <select onChange={(e) => setContinent(e.target.value)}>
+          <option value="all">All</option>
+          {continents.map((continent) => (
+            <option key={continent} value={continent}>
+              {continent}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="error">
+        <p>{error}</p>
+      </div>
+      <div className="data">
+        {filtered.map((country) => (
+          <CountryCard key={country.name} {...country} />
+        ))}
+      </div>
     </main>
   );
 }
--- src/hooks/useCountries.js
+++ src/hooks/useCountries.js
@@ -4,16 +4,20 @@ import { fetchCountries } from '../services/fetchCountries.js';
 export function useCountries() {
   const [countries, setCountries] = React.useState([]);
   const [error, setError] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(true);
   React.useEffect(() => {
+    setIsLoading(true);
+
     const fetchData = async () => {
       try {
         const resp = await fetchCountries();
+        setIsLoading(false);
         setCountries(resp);
-      } catch (e) {
+      } catch (error) {
         setError('An Issue Was Encountered');
       }
     };
     fetchData();
   }, []);
-  return { countries, error };
+  return { countries, error, isLoading };
 }
